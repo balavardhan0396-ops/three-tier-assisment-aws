949d95913b2a2abf94180359523f8a1e7d8dde48
balavardhan0396-ops <balavardhan0396@gmail.com>
2026-08-05 17:50:50 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,21 +1,36 @@
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
-});
+let connection;
 
-connection.connect((err) => {
-  if (err) {
-    console.log("Database Connection Failed");
-    console.log(err);
-    return;
-  }
+function connectDatabase() {
+  connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+  });
 
-  console.log("MySQL Connected Successfully");
-});
+  connection.connect((err) => {
+    if (err) {
+      console.log("❌ MySQL not ready. Retrying in 5 seconds...");
+      setTimeout(connectDatabase, 5000);
+    } else {
+      console.log("✅ MySQL Connected Successfully");
+    }
+  });
 
-module.exports = connection;
+  connection.on("error", (err) => {
+    console.log("Database Error:", err);
+
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      connectDatabase();
+    }
+  });
+}
+
+connectDatabase();
+
+module.exports = {
+  getConnection: () => connection,
+};
